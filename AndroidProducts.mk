6afef7975da62dbf973a8a9365dc4d8e925c1ffd
xiliahz <dujunjie584@gmail.com>
2023-12-18 12:16:00 +0800

    welcome thyme

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_thyme.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_thyme-user \
+    lineage_thyme-userdebug \
+    lineage_thyme-eng
---
+++ lineage_thyme.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from thyme device
+$(call inherit-product, device/xiaomi/thyme/device.mk)
+
+# Inherit common LineageOS configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_thyme
+PRODUCT_DEVICE := thyme
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 10S
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Inherit common LineageOS configurations
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+DEVICE_MAINTAINER := Xiliahz
+
+BUILD_FINGERPRINT := Xiaomi/thyme/thyme:13/RKQ1.211001.001/V14.0.6.0.TGACNXM:user/release-keys
