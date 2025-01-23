c767b46da1637f88d43a97bb4b60466f3c175e83
Fuutao <fuutaoao@gmail.com>
2025-01-23 11:49:38 +0800

    Revert "thyme: Implement DSPVolumeSynchronizer"
    This reverts commit 65382b77311415007105f23e7650822116b0b104.

--- device.mk
+++ device.mk
@@ -41,10 +41,6 @@ TARGET_HAS_UDFPS := true
 PRODUCT_PACKAGES += \
     ThymeNfcOverlay
 
-# DSP Volume Synchronizer
-PRODUCT_PACKAGES += \
-    DSPVolumeSynchronizer
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
